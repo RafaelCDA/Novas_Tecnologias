972f735358486090724e9a0846a4395164f9e1d4
RAFAEL CUTRIM DE ALMEIDA <rafael.cutrim@a.ucb.br>
2024-03-19 10:06:14 -0300

    Atividades

---
+++ atividade1001/main.py
@@ -0,0 +1,4 @@
+a = int(input("Digite o primeiro valor "))
+b = int(input("Digite o Segundo valor "))
+x = a + b 
+print("X=",x,"Renan e gay")
---
+++ atividade1002/main.py
@@ -0,0 +1,4 @@
+n = 3.14159
+a = float(input("Digite o valor do raio "))
+x = n *a
+print(x)
